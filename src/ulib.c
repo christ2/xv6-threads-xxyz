622de979f26fce4112d88449377fe18798b4586c
christian <christian.chandra88@gmail.com>
2023-03-09 17:22:00 +0700

    cobain

--- src/ulib.c
+++ src/ulib.c
@@ -106,14 +106,105 @@ memmove(void *vdst, const void *vsrc, int n)
   return vdst;
 }
 
-int thread_create(void (*start_routine)(void *, void *), void *arg1, void *arg2) {
-  void *stack = malloc(PGSIZE);
-  if (stack == 0)
-    return -1;
-  int r = clone(start_routine, arg1, arg2, stack);
-  if (r == -1)
-    free(stack);
-  return r;
+// int thread_create(void (*start_routine)(void *, void *), void *arg1, void *arg2) {
+//   void *stack = malloc(PGSIZE);
+//   if (stack == 0)
+//     return -1;
+//   int r = clone(start_routine, arg1, arg2, stack);
+//   if (r == -1)
+//     free(stack);
+//   return r;
+// }
+
+#define MAX_THREADS 64
+
+struct thread {
+  void (*func)(void *, void *);
+  void *arg1;
+  void *arg2;
+  void *stack;
+};
+
+struct thread threads[MAX_THREADS];
+char *stack_pool[MAX_THREADS];
+int stack_count = 0;
+
+void init_threading() {
+  int i;
+  for (i = 0; i < MAX_THREADS; i++) {
+    stack_pool[i] = kalloc();
+    if (stack_pool[i] == 0) {
+      panic("Out of memory\n");
+    }
+  }
+}
+
+void *get_stack() {
+  if (stack_count == MAX_THREADS) {
+    return 0;
+  }
+  return stack_pool[stack_count++];
+}
+
+void release_stack(char *stack) {
+  if (stack_count == 0) {
+    panic("Stack pool underflow\n");
+  }
+  stack_pool[--stack_count] = stack;
+}
+
+int thread_create(void (*func)(void *, void *), void *arg1, void *arg2) {
+  struct thread *t;
+  char *stack;
+
+  if (stack_count == 0) {
+    return -1;  // No more stack memory
+  }
+
+  t = &threads[0];
+  while (t->stack != 0 && t < &threads[MAX_THREADS]) {
+    t++;
+  }
+  if (t == &threads[MAX_THREADS]) {
+    return -1;  // No more thread slots
+  }
+
+  stack = get_stack();
+  if (stack == 0) {
+    return -1;  // Out of memory
+  }
+
+  t->func = func;
+  t->arg1 = arg1;
+  t->arg2 = arg2;
+  t->stack = stack;
+
+  return 0;
+}
+
+void thread_exit() {
+  struct thread *t = &threads[0];
+  while (t->stack != 0 && t < &threads[MAX_THREADS]) {
+    t++;
+  }
+  if (t == &threads[MAX_THREADS]) {
+    panic("No thread to exit\n");
+  }
+  release_stack(t->stack);
+  t->stack = 0;
+}
+
+void scheduler() {
+  int i;
+
+  while (1) {
+    for (i = 0; i < MAX_THREADS; i++) {
+      if (threads[i].stack != 0) {
+        threads[i].func(threads[i].arg1, threads[i].arg2);
+        thread_exit();
+      }
+    }
+  }
 }
 
 int thread_join() {
